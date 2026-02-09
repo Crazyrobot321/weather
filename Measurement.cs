11ea070e0940c64438a32234ec8541371e41fdfa
Erik Niva <erikmz40@gmail.com>
2026-02-09 14:07:02 +0100

    Add project files.

---
+++ Measurement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace weather
+{
+    public class Measurement
+    {
+        public DateTime Datum { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace weather
+{
+    internal class Program
+    {
+        public static List<string> lines = new List<string>();
+        public static string path = "../../../Files/";
+        static void Main(string[] args)
+        {
+            FileInput("tempdata.txt");
+            while (true)
+            {
+                Console.WriteLine("1. Utomhus");
+                Console.WriteLine("2. Inomhus");
+                Console.WriteLine("3. Avsluta");
+                Console.Write("Val: ");
+                if(int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            Ute.Ute.Outdoor();
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            return;
+                        default:
+                            Console.WriteLine("Ogiltigt val, försök igen.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Vänligen ange ett giltigt nummer.");
+                }
+            }
+        }
+        
+        static void FileInput(string filename)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(path + filename))
+                {
+                    string line = reader.ReadLine();
+                    while(line != null)
+                    {
+                        if (!line.Contains("2016-05") && !line.Contains("2017-01"))
+                            lines.Add(line);
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("Filen finns inte");
+            }
+        }
+    }
+}
